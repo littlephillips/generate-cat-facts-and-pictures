7a010bedc5bd18a45dbad4d99b0d865b9dc64450
littlephillips <phillipkinuthia72@gmail.com>
2022-06-24 18:40:50 +0300

    removed the functionality of generating cat facts

--- index.js
+++ index.js
@@ -1,84 +1,48 @@
     //eventlisteners
 
-    
     document.addEventListener('DOMContentLoaded', () => {
-    //cat images
-    let catGeneration = document.getElementById("cat-generator");
-    catGeneration.addEventListener('click', generateCats)
-  
-    //cat facts
-    let catFactGenerator = document.getElementById("cat-fact-generator");
-    catFactGenerator.addEventListener('click', generateCatFacts)
-
-    //light modes
-    const buttonSwitch = document.getElementById("modes");
-    buttonSwitch.addEventListener('click', darkMode);
-
-
-  //fetch requests
-
-  function generateCats(){
-    let catsContainer = document.getElementById('cat-image-container');
-    catsContainer.innerHTML = ''
-
-    fetch("https://api.thecatapi.com/v1/images/search")
-    .then(res => res.json())
-    .then(data => {
-      let cats = data[0].url
-      let catImageElem = document.createElement('img')
-      catImageElem.setAttribute('src', `${cats}`)
-
-      let catsContainer = document.querySelector('#cat-image-container')
-      catsContainer.appendChild(catImageElem)
-      catsContainer.style.border = "2px solid #adadaa"
-
-    })
-
-  }
-
-    //generating cat facts
+      
+      let catGeneration = document.getElementById("cat-generator");
+      catGeneration.addEventListener('click', generateCats)
     
-  function generateCatFacts(){
-    let menu = document.getElementById('cat-data-container')
-    menu.innerHTML = ''
-
-    fetch('https://brianiswu-cat-facts-v1.p.rapidapi.com/facts', {
-      method: 'GET',
-      headers: {
-        'X-RapidAPI-Key': 'abd9343adfmsh3034d615139dff9p193554jsn3b451274579c',
-        'X-RapidAPI-Host': 'brianiswu-cat-facts-v1.p.rapidapi.com'
-        },
-    })
-
-    .then(res => res.json())
-    .then(data => {
-      let menu = document.getElementById('cat-data-container')
-      menu.innerHTML = ''
-      for (elem of data){
-          let li = document.createElement('li')
-          li.textContent = elem.text
-          menu.append(li)
-           menu.style.border = "1px solid #adadaa"
+      const buttonSwitch = document.getElementById("modes");
+      buttonSwitch.addEventListener('click', darkMode);
+    
+      //fetch requests
+    
+      function generateCats(){
+        let catsContainer = document.getElementById('cat-image-container');
+        catsContainer.innerHTML = ''
+    
+        fetch("https://api.thecatapi.com/v1/images/search")
+        .then(res => res.json())
+        .then(data => {
+          let cats = data[0].url
+          let catImageElem = document.createElement('img')
+          catImageElem.setAttribute('src', `${cats}`)
+    
+          let catsContainer = document.querySelector('#cat-image-container')
+          catsContainer.appendChild(catImageElem)
+        })
+    
       }
+    
+      //switch light to dark mode
+    
+        function darkMode(e){
+          if(document.body.style.backgroundColor == "rgb(10, 15, 34)"){
+            document.body.style.backgroundColor = "#606A74"
+          } else {
+            document.body.style.backgroundColor = "rgb(10, 15, 34)"
+          }
+    
+          if(buttonSwitch.textContent == "dark mode"){
+            buttonSwitch.textContent = "light mode"
+         } else {
+            buttonSwitch.textContent = "dark mode"
+         }
+    
+        }
+    
     })
-  }
-
-
-  //switch light to dark mode
-  
-
-  function darkMode(e){
-    if(document.body.style.backgroundColor == "rgb(10, 15, 34)"){
-      document.body.style.backgroundColor = "#606A74"
-    } else {
-      document.body.style.backgroundColor = "rgb(10, 15, 34)"
-    }
-
-    if(buttonSwitch.textContent == "light mode"){
-      buttonSwitch.textContent = "dark mode"
-   } else {
-      buttonSwitch.textContent = "light mode"
-   }
-
-  }
-})   
+    
